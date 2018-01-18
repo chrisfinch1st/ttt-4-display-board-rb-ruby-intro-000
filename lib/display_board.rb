9399eb7f17a9b57021b13207aeefae57748301d6
Christopher Finch <christopher.h.finch@gmail.com>
2018-01-18 07:14:26 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
+def display_board(board)
+  puts " #{0} / #{1} / #{2} "
+  puts "-----------"
+  puts " #{3} / #{4} / #{5} "
+  puts "-----------"
+  puts " #{6} / #{7} / #{8} "
+
+entered
+
+  
